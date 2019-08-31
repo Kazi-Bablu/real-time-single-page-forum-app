4821dc670a1e54ff6ced4d3c57aace45a74066ea
kazi-bablu <kazibablubif@gmail.com>
2019-09-01 01:05:55 +0600

    factory and seed

--- database/factories/LikesFactory.php
+++ database/factories/LikesFactory.php
@@ -3,7 +3,10 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Likes::class, function (Faker $faker) {
+
     return [
-        //
+        'user_id' => function () {
+                return \App\User::all()->random();
+        }
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class  DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
@@ -12,5 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(\App\User::class,10)->create();
+        factory(\App\Category::class,5)->create();
+        factory(\App\Question::class,10)->create();
+        factory(\App\Reply::class,50)->create()->each(function ($reply){
+            return $reply->like()->save(factory(\App\Likes::class)->make());
+        });
     }
 }
