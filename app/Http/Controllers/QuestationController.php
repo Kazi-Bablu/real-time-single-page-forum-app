0d31802edb83f9576f65de6f0f60d8fbbcd85618
Kazi-Bablu <kazibablubif@gmail.com>
2019-08-31 21:52:50 +0600

    model , controller, factory, migration option created

---
+++ app/Http/Controllers/QuestationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Questation;
+use Illuminate\Http\Request;
+
+class QuestationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Questation  $questation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Questation $questation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Questation  $questation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Questation $questation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Questation  $questation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Questation $questation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Questation  $questation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Questation $questation)
+    {
+        //
+    }
+}
