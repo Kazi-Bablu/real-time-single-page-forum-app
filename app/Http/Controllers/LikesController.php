da8ccc04d9f6c2fa966142ca3557dc382e4373d6
Kazi Bablu <33626354+Kazi-Bablu@users.noreply.github.com>
2019-09-02 17:26:12 +0600

    like and dislike option created

--- app/Http/Controllers/LikesController.php
+++ app/Http/Controllers/LikesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Likes;
+use App\Reply;
 use Illuminate\Http\Request;
 
 class LikesController extends Controller
@@ -81,5 +82,18 @@ class LikesController extends Controller
     public function destroy(Likes $likes)
     {
         //
+    }
+
+    public function likeIt(Reply $reply)
+    {
+        $reply->like()->create([
+           // 'user_id' =>auth()->id(),
+            'user_id'=>'1'
+        ]);
+    }
+
+    public function unLikeIt(Reply $reply)
+    {
+        $reply->like()->where(['user_id',auth()->id()])->first()->delete();
     }
 }
